4e7890b058522ebf40969a839ab8578157d8c185
ajshirlow7 <ajshirlow@gmail.com>
2025-07-24 18:05:36 +0100

    starting up project foundations. Installing python and connecting to a database

---
+++ MoneyMaker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MoneymakerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MoneyMaker'
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault('SECRET_KEY', 'django-insecure-lrse5pt!2sn4_=n96q_bj7er6o4u24e9u7!-h%k_elfl4ai!pk')
+
+os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_Y2iLFsTG0lfy@ep-small-credit-a2xrpd0x.eu-central-1.aws.neon.tech/taper_wagon_click_629803')
