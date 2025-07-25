d653001d046d4820d63ed1dc05b4c58159e1e972
ajshirlow7 <ajshirlow@gmail.com>
2025-07-25 11:06:46 +0100

    adding a homepage.html that the user sees at the start, adding a login, register button which the user can use to create an account in the database and login to an existing account in the database

--- MoneyMaker/urls.py
+++ MoneyMaker/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import home_page_view 
+from django.urls import path, include
+from .views import home_page_view, register_view
 
 urlpatterns = [
-    path("", home_page_view),
-    ]
+    path("", home_page_view, name="home"),
+    path("register/", register_view, name="register"),
+    path("accounts/", include("django.contrib.auth.urls")),  # This line is required!
+]
--- MoneyMaker/views.py
+++ MoneyMaker/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
-# Create your views here.
 def home_page_view(request):
-    return HttpResponse("<h1>Welcome to the MoneyMaker app!</h1>")
+    print("User:", request.user, "Authenticated:", request.user.is_authenticated)
+    form = UserCreationForm()
+    login_form = AuthenticationForm()
+    return render(request, "homepage.html", {"form": form, "login_form": login_form})
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            # Render homepage with errors if form is invalid
+            return render(request, "homepage.html", {"form": form})
+    else:
+        return redirect("home")
+
