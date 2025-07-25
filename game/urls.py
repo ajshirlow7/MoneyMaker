7c03acf6b86392fa2ce3cc9c4cc858df984e250e
ajshirlow7 <ajshirlow@gmail.com>
2025-07-25 11:37:30 +0100

    added the leadboard page, which shows all users just for now

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import play_view
+from .views import play_view, leaderboard_view
 
 urlpatterns = [
     path('', play_view, name='play'),
+    path('leaderboard/', leaderboard_view, name='leaderboard'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @login_required
 def play_view(request):
     return render(request, "game/play.html")
+
+@login_required
+def leaderboard_view(request):
+    users = User.objects.all().order_by('username')  # You can order by any field you want
+    return render(request, "game/leaderboard.html", {"users": users})
